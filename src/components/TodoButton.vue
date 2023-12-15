515c79d26acd462a5848e91d3fd0f77f7c7361c1
Taus Johana Giselle <JG.Taus@kline.it>
2023-12-15 15:51:56 +0100

    Todos project

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+     <button>
+        <slot>Add</slot>
+    </button>
+</template>
+
+<style lang="scss" scoped>
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <p>Vue todos is a CRUD Todo tracking application created for a Vue 3 crash course. 
+      Track your daily task’s using this todo tracker with abilities to save, complete, edit and delete todos.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
